14fac1ebdad6dad2c85b39b1d2cefe7bf4e5acf0
Gabriel Roy-Lortie <gabriel.roylortie@gmail.com>
2018-07-05 23:09:50 -0400

    Introduces move categories

---
+++ src/ts/editor/behaviors/move-category.ts
@@ -0,0 +1,14 @@
+namespace PRKR.Editor.Behaviors {
+
+  /**
+   * Categories of objects that can be moved toghether. Unclassified moves alone.
+   */
+  export enum MoveCategory {
+
+    Unclassified = 0,
+
+    Area = 1,
+
+    AreaElement = 2
+  }
+}
