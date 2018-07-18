6b496eda16b2ffafee408bb04312178e96a1a3ab
Gabriel Roy-Lortie <gabriel.roylortie@gmail.com>
2018-07-17 22:14:57 -0400

    Adds dimmed color variant in `Colors`

--- src/ts/editor/colors.ts
+++ src/ts/editor/colors.ts
@@ -4,6 +4,11 @@ namespace PRKR.Editor {
     
     static TOOL_SUCCESS_COLOR = 0x00ff00;
 
+    static TOOL_SUCCESS_COLOR_DIM = 0x008800;
+
     static TOOL_ERROR_COLOR = 0xff0000;
+
+    static TOOL_ERROR_COLOR_DIM = 0x880000;
+
   }
 }
