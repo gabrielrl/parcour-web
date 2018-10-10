4b6af27982a4b4ced0cbab08a0e48645282d7508
Gabriel Roy-Lortie <gabriel.roylortie@gmail.com>
2018-10-09 22:16:41 -0400

    Implements copy normalization algorithm

--- src/ts/editor/commands/copy-command.ts
+++ src/ts/editor/commands/copy-command.ts
@@ -1,5 +1,7 @@
 namespace PRKR.Editor.Commands {
 
+  import EditorObject = Objects.EditorObject;
+
   export class CopyCommand implements Command {
 
     constructor(private _editor: ParcourEditor) { }
@@ -21,7 +23,64 @@ namespace PRKR.Editor.Commands {
     get highlighted() { return false; }
 
     run() {
-      // TODO
+
+      let sel = this._editor.selectedObjects;
+      if (sel.length !== 0) {
+
+        sel = this._normalizeSelection(sel);
+
+        console.log('Attempting to put the following in the clipboard', sel);
+        
+        console.log(
+          `It is a collection of ${ sel.length } object${ sel.length > 1 ? 's' : '' }`)
+
+        let objs = sel.map(o => o.model.toObject());
+        objs.forEach(o => delete o.id);
+        let json = JSON.stringify(objs);
+
+        console.log('...JSON=', json);
+
+        //let clip = this._editor.clipboard;
+
+      }
+
     }
+
+    private _normalizeSelection(selection: EditorObject[]) {
+
+      let isArea = x => x.model instanceof Model.Area;
+  
+      // is there an area in the selection.
+      let areaCheck = _.find(selection, isArea);
+      if (areaCheck) {
+  
+        // Work in "area mode".
+        // Collect areas
+        let areas = _.filter(selection, isArea);
+        let augmented = this._extendSelection(areas);
+  
+        return augmented;
+        
+      } else {
+  
+        // Work in "area element mode".
+        return [].concat(selection);
+  
+      }
+    }
+  
+    private _extendSelection(areas: EditorObject[]) {
+  
+      let augmented: EditorObject[] = [].concat(areas);
+
+      areas.forEach(area => {
+        augmented = augmented.concat(this._editor.getObjectsByAreaId(area.id));
+      });
+  
+      return augmented;
+  
+    }
+  
+    
   }
 }
