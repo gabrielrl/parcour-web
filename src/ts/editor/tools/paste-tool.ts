3266e4b1d4354dcc5d0b40cbb268abe4ed941534
Gabriel Roy-Lortie <gabriel.roylortie@gmail.com>
2018-10-16 22:10:07 -0400

    Adds edit step creation and validation on mouse move

--- src/ts/editor/tools/paste-tool.ts
+++ src/ts/editor/tools/paste-tool.ts
@@ -10,6 +10,7 @@ namespace PRKR.Editor.Tools {
 
     private _pastePayload = [];
     private _areaMode: boolean;
+
     private _helpers: BoundingBoxHelper[];
     private _helpersRoot: THREE.Group = null;
 
@@ -59,9 +60,49 @@ namespace PRKR.Editor.Tools {
       let intersect = this._editor.projectMouseOnFloor(new THREE.Vector2(event.clientX, event.clientY));
       if (intersect) {
 
-        // let area = this._editor.getAreaAtLocation(intersect.point);
+        let setValidMaterial = (helper: BoundingBoxHelper) => {
+          helper.setLineMaterial(Constants.Materials.Lines.Valid);
+          helper.setFaceMaterial(Constants.Materials.Faces.Valid);
+        };
+        let setInvalidMaterial = (helper: BoundingBoxHelper) => {
+          helper.setLineMaterial(Constants.Materials.Lines.Invalid);
+          helper.setFaceMaterial(Constants.Materials.Faces.Invalid);
+    };
 
         let target = intersect.point.round();
+
+        let editStep = this._buildEditStep(target);
+        if (editStep) {
+          let validation = this._editor.validateEditStep(editStep);
+          validation.forEach((v, i) => console.log('validation ' + i + ':', v));
+
+          let errors = validation.filter(v => v.level === Validators.ResultLevel.Error);
+          if (errors.length !== 0) {
+            this._helpers.forEach(setInvalidMaterial);
+          } else {
+            this._helpers.forEach(setValidMaterial);
+          }
+
+          // only show the helpers for objects that actualy gets pasted (objects falling outside of all areas are
+          // excluded in `_buildEditStep`).
+          let composed = <EditSteps.ComposedStep>editStep;
+          this._helpers.forEach(helper => {
+            if (!helper.helperFor) {
+              helper.visible = false;
+            } else {
+              let addStep = composed.steps.find(step => {
+                return (
+                  step instanceof EditSteps.AddObjectStep &&
+                  step.data.$$id === helper.helperFor
+                );
+              });
+              helper.visible = addStep != null;
+            }
+          });
+        } else {
+          this._helpers.forEach(setInvalidMaterial);
+          this._helpers.forEach(h => h.visible = true);
+        }
 
         this._helpersRoot.position.copy(target);
         this._helpersRoot.visible = true;
@@ -140,15 +181,21 @@ namespace PRKR.Editor.Tools {
       models.forEach(x => {
         if (x instanceof Model.AreaElement) {
           let pastee = x.toObject();
+
+          // Express location relatively to the payload center
           let newLocation = [
             pastee.location[0] - payloadOrigin.x,
             pastee.location[1] - payloadOrigin.y,
             pastee.location[2] - payloadOrigin.z,
           ];
           pastee.location = newLocation;
+
+          // Get box 
           let pasteeModel = <Model.AreaElement>ParcourObject.fromObject(pastee);
           let box = pasteeModel.getBoundingBox();
           if (box == null) {
+            // Safety net for when the object doesn't offer a bounding box but it's ugly and should not be relyed on.
+            // TODO make bounding box mandatory?
             box = new THREE.Box3(
               pasteeModel.location.clone(),
               M.Vector3.Zero.clone()
@@ -159,12 +206,67 @@ namespace PRKR.Editor.Tools {
             useLines: false,
             faceMaterial: Constants.Materials.Faces.Valid,
             lineMaterial: Constants.Materials.Lines.Valid
-          });
+          }, x.id);
           this._pastePayload.push(pastee);
           this._helpers.push(helper);
           this._helpersRoot.add(helper);
         }
       });
+    }
+
+    /**
+     * Builds an edit step that adds the paste payload to the current parcour at the current user location specified
+     * by `target`.
+     * @param target current user target for the paste payload.
+     */
+    private _buildEditStep(target: Vector3) {
+
+      let payload = [];
+      this._pastePayload.forEach(pastee => {
+
+        let result = _.cloneDeep(pastee);
+
+        if (pastee.id) {
+          result.$$id = pastee.id;
+        }
+
+        if (pastee.id && this._editor.getObjectById(pastee.id) != null) {
+          delete result.id;
+        }
+
+        // Express location from the current target.
+        let newWorldLocation = new Vector3(
+          target.x + pastee.location[0],
+          target.y + pastee.location[1],
+          target.z + pastee.location[2],
+        );
+
+        // Is the new location inside an area?
+        let area = this._editor.getAreaAtLocation(newWorldLocation);
+        if (area) {
+
+          let newLocation = [
+            newWorldLocation.x - area.location.x,
+            newWorldLocation.y - area.location.y,
+            newWorldLocation.z - area.location.z
+          ];
+          
+          result.areaId = area.id;
+          result.location = newLocation;
+          payload.push(result);
+
+        }
+
+      });
+
+      let addSteps = payload.map(pastee => new EditSteps.AddObjectStep(pastee));
+
+      if (addSteps.length !== 0) {
+        return new EditSteps.ComposedStep(addSteps);
+      }
+
+      return null;
+
     }
 
   }
