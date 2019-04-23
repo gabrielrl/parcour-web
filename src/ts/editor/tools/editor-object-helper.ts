b979afa4ba3ad0020987a578786cb538d8f50b3c
Gabriel Roy-Lortie <gabriel.roylortie@gmail.com>
2019-04-22 21:38:04 -0400

    Updates EditorObjectHelper...
    it should now be set at the proxied object's location (no longer the
    pivot location)

--- src/ts/editor/tools/editor-object-helper.ts
+++ src/ts/editor/tools/editor-object-helper.ts
@@ -5,8 +5,8 @@ namespace PRKR.Editor.Tools {
   /**
    * A helper to proxy any editor object used to represent tool effects.
    * 
-   * NOTE Created mesh is always centered at the proxied object's local pivot point. Requied for `setRotateBy` to
-   * work.
+   * Create an instance by specifying an editor object to proxy. Then insert it in the scene and set it at the
+   * object's location. Use `setRotateBy`, `setMoveBy` and `setResizeBy` to update the helper's display.
    */
   export class EditorObjectHelper extends THREE.Object3D {
 
@@ -14,6 +14,12 @@ namespace PRKR.Editor.Tools {
 
     /** Mesh that proxies the original object. */
     private _proxy: THREE.Mesh;
+
+    /** Object to control the rotation of the proxy. Takes the relative location of the object's pivot into account. */
+    private _rotateControl: THREE.Object3D;
+
+    /** Object to control the movement of the proxy. */
+    private _moveControl: THREE.Object3D;
 
     private _restRotation: THREE.Quaternion;
     private _size: THREE.Vector3;
@@ -37,8 +43,6 @@ namespace PRKR.Editor.Tools {
         w = box.max.x - box.min.x;
         h = box.max.y - box.min.y;
         d = box.max.z - box.min.z;
-
-        geometry.translate(pivotAdjustment.x, pivotAdjustment.y, pivotAdjustment.z);
         this._proxy = new THREE.Mesh(geometry, material);
       } else {
         box = object.boundingBox;
@@ -47,13 +51,19 @@ namespace PRKR.Editor.Tools {
         d = box.max.z - box.min.z;
         let g = new THREE.BoxBufferGeometry(w, h, d);
         g.translate(
-          box.min.x + w * .5 + pivotAdjustment.x,
-          box.min.y + h * .5 + pivotAdjustment.y,
-          box.min.z + d * .5 + pivotAdjustment.z
+          box.min.x + w * .5,
+          box.min.y + h * .5,
+          box.min.z + d * .5
         );
         this._proxy = new THREE.Mesh(g, material);
       }
-      this.add(this._proxy);
+      this._proxy.position.copy(pivotAdjustment);
+      this._rotateControl = new THREE.Object3D();
+      this._rotateControl.position.copy(pivotAdjustment).negate();
+      this._rotateControl.add(this._proxy);
+      this._moveControl = new THREE.Object3D();
+      this._moveControl.add(this._rotateControl);
+      this.add(this._moveControl);
 
       this._size = new THREE.Vector3(w, h, d);
       this._restRotation = new THREE.Quaternion();
@@ -68,7 +78,7 @@ namespace PRKR.Editor.Tools {
     }
 
     setMoveBy(move: THREE.Vector3) {
-      this._proxy.position.copy(move);
+      this._moveControl.position.copy(move);
     }
 
     setRestRotation(restRotation: THREE.Quaternion) {
@@ -77,11 +87,11 @@ namespace PRKR.Editor.Tools {
       } else {
         this._restRotation.set(0, 0, 0, 1);
       }
-      this._proxy.quaternion.copy(this._restRotation);
+      this._rotateControl.quaternion.copy(this._restRotation);
     }
 
     setRotateBy(rotation: THREE.Quaternion) {
-      this._proxy.quaternion.copy(this._restRotation).premultiply(rotation);
+      this._rotateControl.quaternion.copy(this._restRotation).premultiply(rotation);
     }
 
     setResizeBy(sizeDelta: THREE.Vector3) {
